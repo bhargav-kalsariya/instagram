7a74f3f0597145ee2df695c7c06aca804d40274a
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2023-06-30 17:17:25 +0530

    signup with bcrypt complete

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const loginSignupRoute = require('./routes/login_signupRoute');
 const { DatabaseConnect } = require('./config/databaseConnect');
-const { LoginSignupRouter } = require('./routes/login_signupRoute');
 
-require('ejs');
 require('dotenv').config();
-
-DatabaseConnect();
+require('ejs');
 
 let app = express();
 
-app.use('view engine', 'ejs');
-app.use('/users', LoginSignupRouter)
-// app.use(express.static('public'));
+DatabaseConnect();
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.listen(process.env.port || 7000, () => {
+//      Routes       // 
+
+app.use('/', loginSignupRoute);
+
+//      port       // 
+
+app.listen(process.env.port, () => {
     console.log('listening on port ' + process.env.port);
-});
+})
--- routes/login_signupRoute.js
+++ routes/login_signupRoute.js
@@ -1,9 +1,29 @@
 const { Router } = require('express');
+const { users } = require('../models/userShema');
+const bcrypt = require('bcrypt');
 
-let LoginSignupRouter = Router();
+let loginSignupRoute = Router();
 
-LoginSignupRouter.get('/signup', (req, res) => {
-    res.send('Welcome')
+loginSignupRoute.get('/index', (req, res) => {
+    res.render('index');
+});
+
+loginSignupRoute.get('/signup', (req, res) => {
+    res.render('signup');
 })
 
-module.exports = { LoginSignupRouter };
+loginSignupRoute.post('/signup', async (req, res) => {
+    let user = await users.findOne({ username: req.body.username });
+    if (!user) {
+        let hashedpassword = await bcrypt.hash(req.body.password, 10)
+        req.body.password = hashedpassword
+        let newUser = await users.create(req.body)
+        res.render('index');
+        console.log(newUser);
+    }
+    else {
+        res.send('user already exists');
+    }
+})
+
+module.exports = loginSignupRoute;
